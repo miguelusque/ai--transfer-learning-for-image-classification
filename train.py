ae3550471a44f1821879543fb1e8965022a7d827
miguelangel <miguelangel@users.noreply.github.com>
2018-05-16 08:46:05 +0200

    Add command line support to train.py

--- train.py
+++ train.py
@@ -1,23 +1,133 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.utils.data as data
+
+
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
 import time
 import copy
+import argparse
 
+# Define command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir')
+parser.add_argument('--gpu', action='store_true', help='Use GPU if available')
+parser.add_argument('--epochs', type=int, help='Number of epochs')
+parser.add_argument('--arch', type=str, help='Model architecture')
+parser.add_argument('--learning_rate', type=float, help='Learning rate')
+parser.add_argument('--hidden_units', type=int, help='Number of hidden units')
 
-def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, scheduler, num_epochs=25):
-    if torch.cuda.is_available():
-        print("Using gpu")
-        device = torch.device("cuda:0")
+args, _ = parser.parse_known_args()
+
+# This method loads and tunes in a model
+#def load_model(arch='vgg19', num_labels=102, hidden_units=4096):
+    
+    
+    
+
+# This method trains a model
+def train_model(image_datasets, arch='vgg19', hidden_units=4096, epochs=25, learning_rate=0.001, gpu=False):
+    # Use command line values when specified
+    if args.arch:
+        arch = args.arch     
+        
+    if args.hidden_units:
+        hidden_units = args.hidden_units
+
+    if args.epochs:
+        epochs = args.epochs
+            
+    if args.learning_rate:
+        learning_rate = args.learning_rate
+
+    if args.gpu:
+        gpu = args.gpu
+        
+    # Using the image datasets, define the dataloaders
+    dataloaders = {
+        x: data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=2)
+        for x in list(image_datasets.keys())
+    }
+ 
+    # Calculate dataset sizes.
+    dataset_sizes = {
+        x: len(dataloaders[x].dataset) 
+        for x in list(image_datasets.keys())
+    }    
+
+        
+    print('Network architecture: ', arch)
+    print('Number of hidden units: ', hidden_units)
+    print('Number of epochs: ', epochs)
+    print('Learning rate: ', learning_rate)
+
+    # Load a pre-trained model
+    if arch=='vgg19':
+        # Load a pre-trained model
+        model = models.vgg19(pretrained=True)
+    elif arch=='alexnet':
+        model = models.alexnet(pretrained=True)
     else:
-        print("Using cpu")
-        device = torch.device("cpu")
+        raise ValueError('Unexpected network architecture', arch)
+        
+    # Freeze its parameters
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    # Features, removing the last layer
+    features = list(model.classifier.children())[:-1]
+  
+    # Number of filters in the bottleneck layer
+    num_filters = model.classifier[len(features)].in_features
 
+    # Extend the existing architecture with new layers
+    num_labels = len(image_datasets['train'].classes)
+    features.extend([
+        nn.Dropout(),
+        nn.Linear(num_filters, hidden_units),
+        nn.ReLU(True),
+        nn.Dropout(),
+        nn.Linear(hidden_units, hidden_units),
+        nn.ReLU(True),
+        nn.Linear(hidden_units, num_labels)
+        #, nn.Softmax() 
+        # Please, notice Softmax layer has not been added as per Pytorch answer:
+        # https://github.com/pytorch/vision/issues/432#issuecomment-368330817
+        # It is not either included in its transfer learning tutorial:
+        # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+    ])
+    
+    model.classifier = nn.Sequential(*features)        
+
+    # Use gpu if selected and available
+    if gpu and torch.cuda.is_available():
+        print('Using GPU for training')
+        device = torch.device("cuda:0")
+        model.cuda()
+        model = nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+    else:
+        print('Using CPU for training')
+        device = torch.device("cpu")     
+
+                
+    # Defining criterion, optimizer and scheduler
+    # Observe that only parameters that require gradients are optimized
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=learning_rate, momentum=0.9)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)    
+        
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    for epoch in range(epochs):
+        print('Epoch {}/{}'.format(epoch, epochs - 1))
         print('-' * 10)
 
         # Each epoch has a training and validation phase
@@ -32,9 +142,11 @@ def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, schedul
             running_corrects = 0
 
             # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
+            for inputs, labels in dataloaders[phase]:                
                 inputs = inputs.to(device)
                 labels = labels.to(device)
+                
+
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -77,3 +189,40 @@ def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, schedul
     model.load_state_dict(best_model_wts)
 
     return model
+
+
+
+
+# Train model if invoked from command line
+if args.data_dir:    
+    # Default transforms for the training, validation, and testing sets
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomRotation(45),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+         ]),
+        'valid': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+         ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+         ])
+    }
+    
+    # Load the datasets with ImageFolder
+    image_datasets = {
+        x: datasets.ImageFolder(root=args.data_dir + '/' + x, transform=data_transforms[x])
+        for x in list(data_transforms.keys())
+    }
+        
+    train_model(image_datasets) 
+    
